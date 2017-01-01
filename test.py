894791171a05ccc26bff0f7b26e2ef4d2d16dde2
romull <romull@amazon.com>
2016-12-31 19:12:56 -0500

    blah

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-def greet_user(username):
-	print("hello user " + username)
+def describe_pet(animal_type, pet_name):
+	print("I have a " + animal_type + " and his name his " + pet_name)
 
-greet_user("Rom")
+describe_pet("dog","harry")
+describe_pet("hamster", "cleo")
+describe_pet(pet_name="rico",animal_type="fish")
