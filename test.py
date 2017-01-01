41475a6d4767601dd8bd58c010d81f29fea307de
romull <romull@amazon.com>
2016-12-31 19:27:56 -0500

    blah

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(animal_type='dog', pet_name='zaza'):
 	print("I have a " + animal_type + " and his name his " + pet_name)
+	return 2
 
 describe_pet("dog","harry")
 describe_pet("hamster", "cleo")
 describe_pet(pet_name="rico",animal_type="fish")
+describe_pet("cat")
+describe_pet(animal_type='lizard')
+describe_pet()
